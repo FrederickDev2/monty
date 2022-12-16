58c24591a8fa94c74ec7bb11000273a3e059db12
FrederickDev2 <108290541+FrederickDev2@users.noreply.github.com>
2022-12-16 10:43:45 +0000

    Update montymain.c

--- montymain.c
+++ montymain.c
@@ -1,53 +1,29 @@
 #include "monty.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-globales_t globalvar = {NULL, NULL, NULL};
+char **op_toks = NULL;
 
 /**
- * main - entry point for the CLI program
- * @argc: count of arguments passed to the program
+ * main - the entry point for Monty Interp
+ *
+ * @argc: the count of arguments passed to the program
  * @argv: pointer to an array of char pointers to arguments
- * Return: EXIT_SUCCESS or EXIT_FAILURE
+ *
+ * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
  */
-
 int main(int argc, char **argv)
 {
-	char *token = NULL;
-	size_t line_buf_size = 0;
-	int line_number = 0, flag = 0, flag2 = 0;
-	ssize_t line_size;
-	stack_t *stack = NULL;
+	FILE *script_fd = NULL;
+	int exit_code = EXIT_SUCCESS;
 
 	if (argc != 2)
-		stderr_usage();
-	globalvar.fd = fopen(argv[1], "r");
-	if (globalvar.fd == NULL)
-		stderr_fopen(argv[1]);
-	line_size = getline(&globalvar.line_buf, &line_buf_size, globalvar.fd);
-	if (globalvar.line_buf[0] == '#')
-		line_size = getline(&globalvar.line_buf, &line_buf_size, globalvar.fd);
-	while (line_size >= 0)
-	{flag = 0;
-		flag2 = 0;
-		line_number++;
-		token = strtok(globalvar.line_buf, DELIM);
-		globalvar.token2 = strtok(NULL, DELIM);
-		if (token == NULL)
-		{flag2 = 1;
-			nop(&stack, line_number); }
-		if (flag2 == 0)
-		{
-			if (token[0] == '#')
-			{
-				line_size = getline(&globalvar.line_buf,
-						    &line_buf_size, globalvar.fd);
-				flag = 1; }}
-		if (flag == 0)
-		{get_builtin(token, &stack, line_number);
-			line_size = getline(&globalvar.line_buf, &line_buf_size,
-					    globalvar.fd); }}
-	free_dlistint(stack);
-	free(globalvar.line_buf);
-	globalvar.line_buf = NULL;
-	fclose(globalvar.fd);
-	return (EXIT_SUCCESS);
+		return (usage_error());
+	script_fd = fopen(argv[1], "r");
+	if (script_fd == NULL)
+		return (f_open_error(argv[1]));
+	exit_code = run_monty(script_fd);
+	fclose(script_fd);
+	return (exit_code);
 }
