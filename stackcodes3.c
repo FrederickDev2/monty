f63728bfc6c958bd813aa1b85cb171df8f2d7ad6
FrederickDev2 <108290541+FrederickDev2@users.noreply.github.com>
2022-12-16 10:43:03 +0000

    Update stackcodes3.c

--- stackcodes3.c
+++ stackcodes3.c
@@ -1,135 +1,59 @@
 #include "monty.h"
 
-/**
- * push - Add node to the stack
- * @stack: head of linkedlist
- * @line_number: line number of the instruction
- *
- * Return: No return
- */
-void push(stack_t **stack, unsigned int line_number)
-{
-
-	int n = 0;
-
-	if (globalvar.token2 == NULL)
-	{
-		free_dlistint(*stack);
-		stderr_int(line_number);
-	}
-	if (!_isdigit() || stack == NULL)
-	{
-		free_dlistint(*stack);
-		stderr_int(line_number);
-	}
-	n = atoi(globalvar.token2);
-	if (*stack  == NULL)
-	{
-		create_node_stackfirst(stack, n);
-	}
-	else
-	{
-		create_node_stackend(stack, n);
-	}
-}
+void monty_nop(stack_t **stack, unsigned int line_number);
+void monty_pchar(stack_t **stack, unsigned int line_number);
+void monty_pstr(stack_t **stack, unsigned int line_number);
 
 /**
- * pall - Print the stack
- * @stack: head of linkedlist
- * @line_number: line number of the instruction
- *
- * Return: No return
+ * monty_nop - Does absolutely nothing for the Monty opcode 'nop'.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
  */
-void pall(stack_t **stack, unsigned int line_number)
-{
-
-	stack_t *temp = NULL;
-
-
-	if (*stack == NULL)
-	{
-		return;
-	}
-	if (*stack == NULL && line_number != 1)
-	{
-		free_dlistint(*stack);
-		free_globalvars();
-		exit(EXIT_SUCCESS);
-	}
-	temp = *stack;
-	while (temp->next != NULL)
-		temp = temp->next;
-	while (temp->prev != NULL)
-	{
-		printf("%d", temp->n);
-		temp = temp->prev;
-		printf("\n");
-	}
-	printf("%d\n", temp->n);
-}
-
-
-/**
- * pint - Print the stack
- * @stack: head of linkedlist
- * @line_number: line number of the instruction
- *
- * Return: No return
- */
-void pint(stack_t **stack, unsigned int line_number)
-{
-
-	stack_t *temp = NULL;
-
-	if (stack == NULL || *stack == NULL)
-	{
-		pint_e(line_number);
-		return;
-	}
-	temp = *stack;
-	while (temp->next != NULL)
-		temp = temp->next;
-
-	printf("%d", temp->n);
-	printf("\n");
-}
-
-/**
- * swap - Print the stack
- * @stack: head of linkedlist
- * @line_number: line number of the instruction
- *
- * Return: No return
- */
-void swap(stack_t **stack, unsigned int line_number)
-{
-	stack_t *temp;
-	int i, j;
-
-	if (*stack == NULL || stack == NULL)
-		op_e(line_number, "swap");
-
-	temp = (*stack)->next;
-	if ((*stack)->next == NULL)
-		op_e(line_number, "swap");
-	while (temp->next != NULL)
-	{
-		temp = temp->next;
-	}
-	i = temp->n;
-	j = temp->prev->n;
-	temp->n = j;
-	temp->prev->n = i;
-}
-/**
- * nop - does not do anything
- * @stack: head of linkedlist
- * @line_number: line number of the instruction
- *
- * Return: No return
- */
-void nop(stack_t **stack, unsigned int line_number)
+void monty_nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
+	(void)line_number;
+}
+
+/**
+ * monty_pchar - Prints the character in the top value
+ *               node of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void monty_pchar(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL)
+	{
+		set_op_tok_error(pchar_error(line_number, "stack empty"));
+		return;
+	}
+	if ((*stack)->next->n < 0 || (*stack)->next->n > 127)
+	{
+		set_op_tok_error(pchar_error(line_number,
+					     "value out of range"));
+		return;
+	}
+
+	printf("%c\n", (*stack)->next->n);
+}
+
+/**
+ * monty_pstr - Prints the string contained in a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void monty_pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = (*stack)->next;
+
+	while (tmp && tmp->n != 0 && (tmp->n > 0 && tmp->n <= 127))
+	{
+		printf("%c", tmp->n);
+		tmp = tmp->next;
+	}
+
+	printf("\n");
+
 	(void)line_number;
 }
